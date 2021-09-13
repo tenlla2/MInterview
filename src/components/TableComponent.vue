40d224ca8f168827d86fad53356a4af165e7898e
Antonio <tenlla2@gmail.com>
2021-09-13 09:03:22 +0200

    Añadidos filtros a ambas tablas, rellenar quotes con datos de characters, acciones controladas y en store y precarga

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,7 +1,101 @@
 <template>
   <div>
+    <v-row v-if="activeFilterCharacter" align="center">
+      <v-col cols="12" md="3">
+        <v-combobox
+          v-model="valFilterRace"
+          :items="listRace"
+          label="Filtro multiselección por raza"
+          multiple
+          chips
+        >
+          <template v-slot:selection="data">
+            <v-chip
+              :key="JSON.stringify(data.item)"
+              v-bind="data.attrs"
+              :input-value="data.selected"
+              :disabled="data.disabled"
+              @click:close="data.parent.selectItem(data.item)"
+            >
+              <v-avatar
+                class="accent white--text"
+                left
+                v-text="data.item.slice(0, 1).toUpperCase()"
+              ></v-avatar>
+              {{ data.item }}
+            </v-chip>
+          </template>
+        </v-combobox>
+      </v-col>
+      <v-col cols="12" md="3">
+        <!-- <v-select
+        class="px-3"
+      label="Filtro de raza" 
+      :items="['Dwarf','Human']"
+      v-model="gender"
+      @change="updateFilterRace"
+    ></v-select> -->
+        <v-combobox
+          v-model="valFilterGender"
+          :items="listGender"
+          label="Filtrar por género"
+          chips
+        >
+          <template v-slot:selection="data">
+            <v-chip
+              :key="JSON.stringify(data.item)"
+              v-bind="data.attrs"
+              :input-value="data.selected"
+              :disabled="data.disabled"
+              @click:close="data.parent.selectItem(data.item)"
+            >
+              <v-avatar
+                class="accent white--text"
+                left
+                v-text="data.item.slice(0, 1).toUpperCase()"
+              ></v-avatar>
+              {{ data.item }}
+            </v-chip>
+          </template>
+        </v-combobox>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-btn @click="resetFilters()"> Resetear </v-btn>
+      </v-col>
+    </v-row>
+    <v-row align="center" v-if="activeFilterQuote">
+      <v-col cols="12" md="3">
+        <v-combobox
+          v-model="valFilterMovie"
+          :items="listMovies"
+          label="Filtrar por película"
+          chips
+        >
+          <template v-slot:selection="data">
+            <v-chip
+              :key="JSON.stringify(data.item)"
+              v-bind="data.attrs"
+              :input-value="data.selected"
+              :disabled="data.disabled"
+              @click:close="data.parent.selectItem(data.item)"
+            >
+              <v-avatar
+                class="accent white--text"
+                left
+                v-text="data.item.slice(0, 1).toUpperCase()"
+              ></v-avatar>
+              {{ data.item }}
+            </v-chip>
+          </template>
+        </v-combobox>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-btn @click="resetFilterMovie()"> Resetear </v-btn>
+      </v-col>
+    </v-row>
     <v-card elevation="10">
       <v-data-table
+        @click:row="clickRow"
         :headers="headers"
         :items="dataColumns"
         class="elevation-1"
@@ -46,28 +140,67 @@
             <v-btn @click="resizeColumn">Activar columnas resizables</v-btn>
           </v-toolbar></template
         >
-        <!-- /* eslint-disable vue/no-unused-vars */ -->
-
-        <template
-          v-for="(header, index) in headers"
-          v-slot:[`header.${header.value}`]
-        >
-          <span :key="index"
+        <template v-for="header in headers" v-slot:[`header.${header.value}`]>
+          <span :key="header._id"
             >{{ header.text }}
             <v-icon @click="hideColumn(header)" color="red lighten-3">
               mdi-eye-off
             </v-icon></span
           >
         </template>
+        <template v-if="actions" v-slot:item.actions="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon class="mr-2" small @click="deleteItem(item)">
+            mdi-delete
+          </v-icon>
+          <v-icon small @click="copyItem(item)"> mdi-content-copy </v-icon>
+        </template>
       </v-data-table>
+      <v-pagination
+        v-show="pagination"
+        class="my-5 pb-5"
+        v-on:input="updateCurrentPage"
+        v-model="currentPage"
+        :length="pageCount"
+        :total-visible="10"
+      ></v-pagination>
+      <v-dialog v-model="myDialog" max-width="500px">
+        <v-card>
+          <v-card-title>
+            <span class="headline">Editor de frases</span>
+          </v-card-title>
+
+          <v-card-text>
+            <v-container>
+              <v-form @submit.prevent="submit">
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="editedItem.dialog"
+                      label="Nombre"
+                      :rules="[(v) => !!v || 'La frase no puede estar vacía']"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="red" text @click="close"> Cancelar </v-btn>
+                  <v-btn color="primary" text @click="save(editedItem)">
+                    Guardar
+                  </v-btn>
+                </v-card-actions>
+              </v-form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
     </v-card>
   </div>
 </template>
 
 <script>
-/* eslint-disable vue/no-unused-vars */
-/* eslint-disable vue/require-v-for-key */
-
 export default {
   name: "TableComponent",
   props: {
@@ -80,21 +213,115 @@ export default {
     tableName: String,
     isHideColumn: Boolean,
     id: String,
+    activeFilterCharacter: Boolean,
+    listRace: Array,
+    raceFilter: Array,
+    listGender: Array,
+    genderFilter: String,
     hideFooter: Boolean,
+    activeFilterQuote: Boolean,
+    movieFilter: String,
+    listMovies: Array,
+    pagination: Boolean,
+    actions: Boolean,
   },
   data: function () {
     return {
+      currentPage: this.page,
       resized: false,
+      editedItem: {},
+      myDialog: false,
     };
   },
-
+  computed: {
+    valFilterRace: {
+      get() {
+        return this.raceFilter;
+      },
+      set(val) {
+        this.$emit("updateFilterRace", val);
+      },
+    },
+    valFilterGender: {
+      get() {
+        return this.genderFilter;
+      },
+      set(val) {
+        this.$emit("updateFilterGender", val);
+      },
+    },
+    valFilterMovie: {
+      get() {
+        return this.movieFilter;
+      },
+      set(val) {
+        this.$emit("updateFilterMovie", val);
+      },
+    },
+  },
   methods: {
+    updateCurrentPage() {
+      this.$emit("updateCurrentPage", this.currentPage);
+    },
+    clickRow(value) {
+      this.$emit("clickRow", value);
+    },
     hideColumn(column) {
       this.$emit("hideColumn", column.value);
     },
     showColumns() {
       this.resized = false;
       this.$emit("showColumns", false);
+    },
+    // updateFilterRace() {
+    //   this.$emit("updateFilterRace", this.raceFilter);
+    // },
+    resetFilters() {
+      this.$emit("resetFilters");
+    },
+    resetFilterMovie() {
+      this.$emit("resetFilterMovie");
+    },
+    copyItem(item) {
+      item.action = "copy";
+      this.updateChanges(item);
+
+      item = JSON.stringify(item);
+      alert("axiosInstance.post(urlBack/copyItem,{" + item + "})");
+    },
+    deleteItem(item) {
+      item.action = "delete";
+      this.updateChanges(item);
+      item = JSON.stringify(item);
+      alert("axiosInstance.post(urlBack/deleteItem,{" + item + "})");
+    },
+    editItem(item) {
+      this.myDialog = true;
+      item.lastValue = item.dialog;
+      this.editedItem = item;
+    },
+    close() {
+      this.editedItem.dialog = this.editedItem.lastValue;
+      this.myDialog = false;
+    },
+    save(item) {
+      item.action = "modify";
+      this.updateChanges(item);
+      this.myDialog = false;
+    },
+    updateChanges(item) {
+      item.date = new Date();
+      item.table = "Quotes";
+
+      if (localStorage.getItem("changes") == null) {
+        let newArray = [];
+        newArray.push(item);
+        localStorage.setItem("changes", JSON.stringify(newArray));
+      } else {
+        var old_changes = JSON.parse(localStorage.getItem("changes"));
+        old_changes.push(item);
+        localStorage.setItem("changes", JSON.stringify(old_changes));
+      }
     },
     resizeColumn() {
       if (!this.resized) {
--- src/views/TableDisplay.vue
+++ src/views/TableDisplay.vue
@@ -17,7 +17,38 @@
           v-on:showColumns="showColumnsCharacter"
           :isHideColumn="isHideColumnCharacters"
           :id="'tablaCharacters'"
-          :hideFooter="false"
+          :activeFilterCharacter="true"
+          v-on:updateFilterRace="updateFilterRace"
+          v-on:updateFilterGender="updateFilterGender"
+          v-on:resetFilters="resetFiltersCharacters"
+          v-on:clickRow="clickRow"
+          :listRace="listRace"
+          :raceFilter="raceFilterStr"
+          :listGender="listGender"
+          :genderFilter="genderFilterStr"
+          :pagination="true"
+        />
+      </v-col>
+      <v-col cols="12">
+        <table-component
+          :dataColumns="dataColumnsQuote"
+          :headers="headersQuotes"
+          :loading="loadingQuotes"
+          :pageCount="numberOfPagesQuotes"
+          :tableName="'FRASES'"
+          :page="pageQuotes"
+          v-on:updateCurrentPage="updateCurrentPageQuotes"
+          v-on:hideColumn="hideColumnQuotes($event)"
+          v-on:showColumns="showColumnsQuote"
+          :isHideColumn="isHideColumnQuotes"
+          :id="'tableQuotes'"
+          :activeFilterQuote="true"
+          v-on:updateFilterMovie="updateFilterMovie"
+          :movieFilter="movieFilterStr"
+          :listMovies="listMovies"
+          :pagination="pagination"
+          v-on:resetFilterMovie="resetFilterMovie"
+          :actions="true"
         />
       </v-col>
     </v-row>
@@ -45,7 +76,30 @@ export default {
       totalRowsCharacters: 0,
       numberOfPagesCharacters: 0,
       itemsPerPageCharacters: 10,
+      dataColumnsQuote: [],
+      headersQuotes: [],
+      originalHeaderQuotes: [],
+      loadingQuotes: false,
+      optionsQuotes: {},
+      pageQuotes: 1,
+      totalRowsQuotes: 0,
+      numberOfPagesQuotes: 0,
+      itemsPerPageQuotes: 10,
       isHideColumnCharacters: false,
+      isHideColumnQuotes: false,
+      raceFilterStr: null,
+      listRace: [],
+      listGender: [],
+      listMovies: [],
+      movieFilterStr: null,
+      genderFilterStr: null,
+      selectedRow: null,
+      idMovie: null,
+      axuDataColumnQuote: [],
+      pagination: true,
+      preload: [],
+      isPreload: false,
+      preloadPageNumber: 1,
     };
   },
   async created() {
@@ -65,6 +119,54 @@ export default {
       "totalQuotesPages",
       "totalQuotes",
     ]),
+    filteredByRaceAndGender() {
+      return this.characters.list.filter((i) => {
+        if (this.raceFilterStr == null && this.genderFilterStr) {
+          if (this.genderFilterStr == "Todos") {
+            return i;
+          } else {
+            if (i.gender == this.genderFilterStr) {
+              return i;
+            }
+          }
+        } else if (this.raceFilterStr != null && this.genderFilterStr == null) {
+          if (this.raceFilterStr.includes(i.race)) {
+            return i;
+          }
+        } else if (this.raceFilterStr != null && this.genderFilterStr != null) {
+          if (this.genderFilterStr == "Todos") {
+            if (this.raceFilterStr.includes(i.race)) {
+              return i;
+            }
+          } else {
+            if (
+              this.raceFilterStr.includes(i.race) &&
+              i.gender == this.genderFilterStr
+            ) {
+              return i;
+            }
+          }
+        }
+      });
+    },
+    filterQuoteCharacter() {
+      return this.totalQuotes.filter((quote) => {
+        if (quote.character == this.selectedRow._id) {
+          return quote;
+        }
+      });
+    },
+    filterByFilm() {
+      return this.axuDataColumnQuote.filter((row) => {
+        if (this.movieFilterStr == "Todos") {
+          return row;
+        } else {
+          if (row.movie == this.idMovie) {
+            return row;
+          }
+        }
+      });
+    },
   },
   methods: {
     update(number) {
@@ -75,7 +177,14 @@ export default {
       await this.getDataApi("character", "");
       await this.getDataApi("quote", "");
       await this.getDataApi("movie", "");
+
       this.setHeaderCharacter();
+      this.getListRace();
+      this.getListGender();
+
+      this.setHeaderQuotes();
+      this.getTotalQuotes();
+      this.getListMovies();
     },
     setHeaderCharacter() {
       let obj = [];
@@ -86,6 +195,46 @@ export default {
       this.originalHeaderCharacters = obj;
     },
 
+    async getListRace() {
+      var aux = ["Todos"];
+      await this.characters.list.map((map) => {
+        if (!aux.includes(map.race)) {
+          aux.push(map.race);
+        }
+      });
+      this.listRace = aux;
+    },
+    async getListGender() {
+      var aux = ["Todos"];
+      await this.characters.list.map((map) => {
+        if (!aux.includes(map.gender) && map.gender != null) {
+          aux.push(map.gender);
+        }
+      });
+      this.listGender = aux;
+    },
+    async getListMovies() {
+      var aux = ["Todos"];
+      await this.movies.list.map((map) => {
+        if (!aux.includes(map.name) && map.name != null) {
+          aux.push(map.name);
+        }
+      });
+      this.listMovies = aux;
+    },
+    setHeaderQuotes() {
+      let obj = [];
+      Object.entries(this.quotes.list[0]).forEach(([key]) => {
+        if (key == "dialog") {
+          obj.push({ text: key, sortable: true, value: key });
+        } else {
+          obj.push({ text: key, sortable: false, value: key });
+        }
+      });
+      obj.push({ text: "Acciones", value: "actions", sortable: false });
+      this.headersQuotes = obj;
+      this.originalHeaderQuotes = obj;
+    },
     async getDataApi(endPoint, paramEndPoint) {
       if (endPoint == "character") {
         this.loadingCharacters = true;
@@ -98,6 +247,23 @@ export default {
         this.numberOfPagesCharacters = this.totalCharacterPages;
         let column = this.getDataColumnCharacter();
         this.dataColumnsCharacter = column;
+      } else if (endPoint == "quote") {
+        this.loadingQuotes = true;
+        await this.getDataAndSet({
+          url: endPoint + paramEndPoint,
+          name: endPoint + "s",
+        });
+        this.loadingQuotes = false;
+        this.numberOfPagesQuotes = this.totalQuotesPages;
+        let column = this.getDataColumnQuote();
+        this.dataColumnsQuote = column;
+        this.axuDataColumnQuote = column;
+        await this.preloadPage();
+      } else {
+        await this.getDataAndSet({
+          url: endPoint + paramEndPoint,
+          name: endPoint + "s",
+        });
       }
     },
 
@@ -110,8 +276,94 @@ export default {
         "?offset=" + this.pageCharacters + "&limit=10"
       );
       this.loadingCharacters = false;
+      this.listRace = [];
+      this.getListRace();
+      this.raceFilterStr = null;
+      this.genderFilterStr = null;
     },
+    async updateCurrentPageQuotes(event) {
+      this.pageQuotes = event;
+      console.log("x:" + this.pageQuotes, "y:" + this.preloadPageNumber);
 
+      if (this.pageQuotes == this.preloadPageNumber) {
+        this.dataColumnsQuote = this.preload;
+        await this.preloadPage();
+      } else if (this.pageQuotes < this.preloadPageNumber) {
+        this.loadingQuotes = true;
+        this.dataColumnsQuote = [];
+        await this.getDataApi("quote", "?page=" + this.pageQuotes);
+        this.loadingQuotes = false;
+        this.movieFilterStr = null;
+      }
+    },
+    updateFilterRace(event) {
+      if (event.length > 0) {
+        if (event.includes("Todos")) {
+          this.raceFilterStr = event;
+          this.dataColumnsCharacter = this.getDataColumnCharacter();
+        } else {
+          this.raceFilterStr = event;
+          this.dataColumnsCharacter = this.filteredByRaceAndGender;
+        }
+      } else {
+        this.raceFilterStr = event;
+        this.dataColumnsCharacter = this.getDataColumnCharacter();
+      }
+    },
+    async preloadPage() {
+      if (this.pageQuotes < this.numberOfPagesQuotes) {
+        var response = await this.getData({
+          url: "quote?page=" + (parseInt(this.pageQuotes) + 1),
+        });
+        this.preload = response.data.docs;
+        this.isPreload = true;
+        this.preloadPageNumber = response.data.page;
+        console.log(
+          "inside : ",
+          this.preload,
+          this.pageQuotes,
+          this.preloadPageNumber
+        );
+      }
+
+      // this.preload =
+    },
+    updateFilterGender(event) {
+      this.genderFilterStr = event;
+      this.dataColumnsCharacter = this.filteredByRaceAndGender;
+    },
+    updateFilterMovie(event) {
+      if (event == "Todos") {
+        this.movieFilterStr = event;
+      } else {
+        let idMovie = this.movies.list.filter((row) => {
+          if (row.name == event) {
+            return row;
+          }
+        });
+        this.pagination = false;
+        this.idMovie = idMovie[0]._id;
+        this.movieFilterStr = idMovie[0].name;
+      }
+      this.dataColumnsQuote = this.filterByFilm;
+    },
+    filterColumnQuote(event) {
+      this.dataColumnsQuote = this.dataColumnsQuote.map((column) => {
+        delete column[event];
+        return column;
+      });
+    },
+    resetFiltersCharacters() {
+      this.raceFilterStr = null;
+      this.genderFilterStr = null;
+      this.dataColumnsCharacter = this.getDataColumnCharacter();
+    },
+    resetFilterMovie() {
+      this.movieFilterStr = null;
+      this.dataColumnsQuote = this.getDataColumnQuote();
+      this.pagination = true;
+    },
+    filterColumnCharacters() {},
     hideColumnCharacter(event) {
       this.isHideColumnCharacters = true;
       this.headersCharacters = this.headersCharacters.filter((row) => {
@@ -119,12 +371,37 @@ export default {
       });
       this.isHideColumnCharacters = true;
     },
-
+    hideColumnQuotes(event) {
+      this.isHideColumnQuotes = true;
+      this.headersQuotes = this.headersQuotes.filter((row) => {
+        return row.value !== event;
+      });
+    },
+    clickRow(value) {
+      this.selectedRow = value;
+      let filtro = this.filterQuoteCharacter;
+      this.axuDataColumnQuote = filtro;
+      this.dataColumnsQuote = filtro;
+      if (this.movieFilterStr != null) {
+        this.dataColumnsQuote = this.filterByFilm;
+      }
+      this.pagination = false;
+    },
     showColumnsCharacter() {
       this.isHideColumnCharacters = false;
       this.headersCharacters = this.originalHeaderCharacters;
     },
-
+    showColumnsQuote() {
+      this.isHideColumnQuotes = false;
+      this.headersQuotes = this.originalHeaderQuotes;
+    },
+    getDataColumnQuote() {
+      let column = [];
+      this.quotes.list.map((quote) => {
+        column.push(quote);
+      });
+      return column;
+    },
     getDataColumnCharacter() {
       let column = [];
       this.characters.list.map((character) => {
