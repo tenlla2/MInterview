ebdf7fccf6644a4e7f82457cb2a536c7efbe83a4
Antonio <tenlla2@gmail.com>
2021-09-13 09:05:51 +0200

    Creada y configurada vista del historial de peticiones

--- src/views/HistoryChanges.vue
+++ src/views/HistoryChanges.vue
@@ -1,5 +1,76 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-row jutify="center">
+      <v-col cols="12">
+        <h1 class="py-4 mb-3 text-center primary white--text">
+          Historial de peticiones
+        </h1>
+        <table-component
+          :loading="loading"
+          :tableName="'Historial'"
+          :dataColumns="items"
+          :headers="headers"
+          :isHideColumn="isHideColumn"
+          v-on:hideColumn="hideColumn($event)"
+          v-on:showColumns="showColumns"
+          :id="'tableHisoty'"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+<script>
+import TableComponent from "../components/TableComponent.vue";
+
+export default {
+  name: "HistoryChanges",
+  components: { TableComponent },
+  data: function () {
+    return {
+      headers: [
+        { text: "Fecha", sortable: true, value: "date" },
+        { text: "Diálogo", sortable: false, value: "dialog" },
+        { text: "Acción", sortable: false, value: "action" },
+        { text: "Personaje", sortable: false, value: "character" },
+        { text: "Película ID", sortable: false, value: "movie" },
+        { text: "Tabla", sortable: false, value: "table" },
+        { text: "ID", sortable: false, value: "_id" },
+      ],
+      originalHeaders: [
+        { text: "Fecha", sortable: true, value: "date" },
+        { text: "Diálogo", sortable: false, value: "dialog" },
+        { text: "Acción", sortable: false, value: "action" },
+        { text: "Personaje", sortable: false, value: "character" },
+        { text: "Película ID", sortable: false, value: "movie" },
+        { text: "Tabla", sortable: false, value: "table" },
+        { text: "ID", sortable: false, value: "_id" },
+      ],
+      items: [],
+      loading: false,
+      isHideColumn: false,
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.loading = true;
+      this.items = JSON.parse(localStorage.getItem("changes"));
+      this.loading = false;
+      console.log(this.items);
+    },
+    hideColumn(event) {
+      this.isHideColumn = true;
+      this.headers = this.headers.filter((row) => {
+        return row.value !== event;
+      });
+      this.isHideColumn = true;
+    },
+    showColumns() {
+      this.isHideColumn = false;
+      this.headers = this.originalHeaders;
+    },
+  },
+};
+</script>
