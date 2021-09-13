14381f95a481cce8681081c1f28f6ef05ec876a2
Antonio <tenlla2@gmail.com>
2021-09-13 07:40:08 +0200

    Menu component finish

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,24 @@
-<template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <template>
+  <div id="app">
+    <v-app>
+      <v-main>
+        <router-view style="margin-bottom:50px" />
+        <menu-bottom />
+      </v-main>
+    </v-app>
+  </div>
 </template>
-
 <script>
-
+import MenuBottom from './components/MenuBottom.vue'
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  name:"App",
+  components:{
+    MenuBottom
+  }
+}
 </script>
+<style scoped>
+#app{
+  background: #f7f7f7;
+}
+</style>
---
+++ src/components/MenuBottom.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-bottom-navigation align=center :grow="true" justify=center fixed width="100%" class="primary--text">
+    <v-btn to="/" color="transparent">
+      <span>Principal</span>
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+    <v-btn color="transparent" to="/history-changes">
+      <span>Historial</span>
+      <v-icon>mdi-clipboard-file</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  name: "MenuBottom",
+  data: () => ({}),
+};
+</script>
+<style>
+.v-bottom-navigation{
+  align-items: center;
+}
+</style>
